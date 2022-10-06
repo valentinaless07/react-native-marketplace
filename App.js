05ef7362568f7e8abe2663c61f341e3456155805
Valentín Alessandrini Zapata <79989652+valentinaless07@users.noreply.github.com>
2022-10-06 15:00:54 -0300

    fonts loaded

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { useFonts } from 'expo-font';
+import * as Font from "expo-font";
+import Apploading from "expo-app-loading";
 import Details from './screens/Details';
 import Home from './screens/Home';
+import { useState } from 'react';
 
 const Stack = createStackNavigator();
 
@@ -14,8 +16,21 @@ const theme = {
   }
 }
 
+
 const App = () => {
 
+  const [fontsLoaded, setFontsLoaded] = useState(false)
+
+  const getFonts = () =>
+  Font.loadAsync({
+    InterBold: require('./assets/fonts/Inter-Bold.ttf'),
+    InterSemiBold: require('./assets/fonts/Inter-SemiBold.ttf'),
+    InterMedium: require('./assets/fonts/Inter-Medium.ttf'),
+    InterLight: require('./assets/fonts/Inter-Light.ttf'),
+    InterRegular: require('./assets/fonts/Inter-Regular.ttf')
+  });
+  
+  if(fontsLoaded){
   return (
     <NavigationContainer theme={theme}>
       <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Home">
@@ -24,6 +39,18 @@ const App = () => {
       </Stack.Navigator>
     </NavigationContainer>
   );
+  }
+  else{
+    return (
+      <Apploading
+        startAsync={getFonts}
+        onFinish={() => {
+          setFontsLoaded(true);
+        }}
+        onError={console.warn}
+      />
+    );
+  }
 }
 
 export default App
