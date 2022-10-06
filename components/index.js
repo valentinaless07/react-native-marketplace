c89efb3d8443e5c220c6031af5bef63a531372e4
Valentín Alessandrini Zapata <79989652+valentinaless07@users.noreply.github.com>
2022-10-06 15:00:04 -0300

    index.js components added

---
+++ components/index.js
@@ -0,0 +1,10 @@
+import FocusedStatusBar from "./FocusedStatusBar";
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+import {CircleButton, RectButton} from "./Button"
+import DetailsBid from "./DetailsBid";
+import DetailsDesc from "./DetailsDesc";
+
+export {
+    HomeHeader, NFTCard, FocusedStatusBar, CircleButton, RectButton, DetailsBid, DetailsDesc
+}
